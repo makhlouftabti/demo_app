248f507d63089fc3d76cd9cc13d59cb7d3c8d5f8
makhlouftabti <125218452+makhlouftabti@users.noreply.github.com>
2023-04-09 18:28:39 +0100

    Add files via upload

---
+++ demo_app/demo_app/demo_app/doctype/pret/pret.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, mqkhlouf and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pret", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ demo_app/demo_app/demo_app/doctype/restitutions/test_restitutions.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, mqkhlouf and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRestitutions(FrappeTestCase):
+	pass
